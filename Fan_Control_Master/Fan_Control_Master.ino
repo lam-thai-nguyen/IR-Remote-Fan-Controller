4b86bee358b9f57f13f2636d913e53085716bf6a
Lam Thai Nguyen <thainguyen2893@gmail.com>
2025-05-17 16:09:28 +0700

    decode from scratch instead of using <IRremote.hpp>

--- Fan_Control_Master/Fan_Control_Master.ino
+++ Fan_Control_Master/Fan_Control_Master.ino
@@ -1,10 +1,8 @@
-#include <IRremote.hpp>
-
 #define IR_RECEIVER_PIN 7
 
 void setup() {
   Serial.begin(9600);
-  IrReceiver.begin(IR_RECEIVER_PIN, ENABLE_LED_FEEDBACK); // Start the receiver
+  pinMode(IR_RECEIVER_PIN, INPUT);
 }
 
 void loop() {
